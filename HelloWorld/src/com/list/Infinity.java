94a03a2e2c4a4f08c0505c7ddd7271ff5b6ed58f
narasimhagk <gkumar301099@gmail.com>
2023-10-31 11:53:50 +0530

    JAVA

---
+++ HelloWorld/src/com/list/Infinity.java
@@ -0,0 +1,10 @@
+package com.list;
+
+public class Infinity {
+	public static void main(String[] args) {
+		for(;;) {
+			System.out.println("Failure");
+		}
+
+	}
+}
---
+++ HelloWorld/src/com/list/Ternary.java
@@ -0,0 +1,13 @@
+package com.list;
+
+public class Ternary {
+ public static void main(String[] args) {
+	 int a=10;
+	 int b;
+	 b=(a==1)?20:30;
+	 System.out.println(b);
+	 b=(a==10)?20:30;
+	 System.out.println(b);
+	 System.out.println(b);
+ }
+}
