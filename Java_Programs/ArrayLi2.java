b04848edb8822876dd40a76254d555b9d330301a
narasimhagk <gkumar301099@gmail.com>
2023-12-20 20:30:27 +0530

    JAVA

---
+++ Java_Programs/ArrayLi2.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class ArrayLi2 {
+    public static void main(String[] args){
+        ArrayList<String>lit=new ArrayList<String>();
+        lit.add("Luffy");
+        lit.add("Zoro");
+        lit.add("Sanji");
+        lit.add("Jimbe");
+        for(String str:lit){
+            System.out.println(str);
+        }
+    }
+}
